c8b6ee54187db1fb4a6e230970317316e9c0bbe9
guyera <thegraygrimm@graymansurvivaltech.com>
2025-07-23 12:25:17 -0400

    init commit

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const { default: mongoose } = require('mongoose')
+const morgan = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
