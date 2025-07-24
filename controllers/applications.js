74763f45a0708c6506ccff4fb7f71d6123bb0cf7
guyera <thegraygrimm@graymansurvivaltech.com>
2025-07-24 15:00:04 -0400

    add application to index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -7,9 +7,14 @@ const User = require('../models/user.js');
 
 // we will build out our router logic here
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
+       const currentUser = await User.findById(req.session.user._id);
+       
+       
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        });
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -31,7 +36,7 @@ router.post('/', async (req, res) => {
         await currentUser.save();
         
         res.redirect(`/users/${currentUser._id}/applications`)
-        
+
     } catch (error) {
         console.log(error);
         res.redirect('/')
