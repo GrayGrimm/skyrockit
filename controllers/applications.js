3bbaca924323f8fbb484a3fea92be63b29ed5d37
guyera <thegraygrimm@graymansurvivaltech.com>
2025-07-24 16:03:12 -0400

    add delete functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -9,9 +9,9 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-       const currentUser = await User.findById(req.session.user._id);
-       
-       
+        const currentUser = await User.findById(req.session.user._id);
+
+
         res.render('applications/index.ejs', {
             applications: currentUser.applications,
         });
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 });
 
 // GET for /new
-router.get('/new', async (req, res) =>{
+router.get('/new', async (req, res) => {
     res.render('applications/new.ejs')
 })
 
@@ -30,18 +30,18 @@ router.get('/new', async (req, res) =>{
 router.post('/', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id)
-        
+
         currentUser.applications.push(req.body)
-        
+
         await currentUser.save();
-        
+
         res.redirect(`/users/${currentUser._id}/applications`)
 
     } catch (error) {
         console.log(error);
         res.redirect('/')
     }
-})
+});
 
 // GET to /applications/:applicationId
 router.get('/:applicationId', async (req, res) => {
@@ -58,6 +58,23 @@ router.get('/:applicationId', async (req, res) => {
         console.log(error);
         res.redirect('/');
     }
-})
+});
+
+// DELETE /applications/:applicationId
+
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+
+        currentUser.save();
+
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    };
+});
 
 module.exports = router;
